8452ddd48ffa6e821f131abb382bf31afcc6ce7d
Hafiz <hafizmuhammadisrar5@gmail.com>
2024-11-12 10:18:07 +0500

    vercel.json

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const route = require("./routes/booksRoute"); // Adjusted path
+const route = require("./routes/booksRoute.js");
 
 const app = express();
 
