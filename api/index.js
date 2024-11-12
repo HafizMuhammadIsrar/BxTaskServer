a8d28ea471ff8c1f95245c0ed2f4b25920db252c
Hafiz <hafizmuhammadisrar5@gmail.com>
2024-11-12 10:09:33 +0500

    vercel.json

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
+const serverless = require("serverless-http");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const route = require("./routes/booksRoute.js");
+const route = require("../routes/booksRoute.js"); // Adjusted path
 
 const app = express();
 
@@ -12,15 +13,13 @@ app.use(cors());
 
 const CONNECTION_URL = `mongodb+srv://misrark414:Vwe4mtiZGLSkJYc2@cluster0.ocf8e.mongodb.net/`;
 
-const PORT = process.env.PORT || 5000;
-
 mongoose
   .connect(CONNECTION_URL)
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port : ${PORT}`));
-  })
-  .catch((error) => {
-    console.error("Error connecting to the database:", error.message);
-  });
+  .then(() => console.log("Database connected successfully"))
+  .catch((error) =>
+    console.error("Error connecting to the database:", error.message)
+  );
 
-app.use("/api/books", route); // Ensure the correct route path
+app.use("/api/books", route);
+
+module.exports.handler = serverless(app);
